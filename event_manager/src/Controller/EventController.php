f2ee34f9ddade6ea835d2704d24b7da7929c4fb9
alamraghu <alamraghu@gmail.com>
2026-04-23 20:57:08 -0400

    Refactor EventController to use EventStorage service

--- event_manager/src/Controller/EventController.php
+++ event_manager/src/Controller/EventController.php
@@ -3,19 +3,40 @@
 namespace Drupal\event_manager\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\event_manager\Service\EventStorage;
 
 class EventController extends ControllerBase {
 
+  protected $storage;
+
+  public function __construct(EventStorage $storage) {
+    $this->storage = $storage;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('event_manager.storage')
+    );
+  }
+
   public function list() {
+    $rows = [];
+
+    foreach ($this->storage->getAll() as $record) {
+      $rows[] = [
+        $record->id,
+        $record->name,
+        $record->email,
+        $record->event,
+        date('Y-m-d H:i', $record->created),
+      ];
+    }
+
     return [
-      '#markup' => '
-        <h2>Upcoming Events</h2>
-        <ul>
-          <li>Drupal Workshop</li>
-          <li>React Bootcamp</li>
-          <li>API Development</li>
-        </ul>
-      ',
+      '#type' => 'table',
+      '#header' => ['ID', 'Name', 'Email', 'Event', 'Created'],
+      '#rows' => $rows,
     ];
   }
 }
